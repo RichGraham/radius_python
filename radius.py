7d248def8746178b5362d2af04b78a099dabc418
Richard Graham <Richard.Graham@nottingham.ac.uk>
2022-11-28 09:41:32 +0000

    Fixed method for computing powers and other minor changes

--- radius.py
+++ radius.py
@@ -1,14 +1,15 @@
+import numpy as np
 def calculate_radius( area):
-    pi = 3.14159
+    pi = np.pi
 
     # Calculate the square root by a Taylor series in powers of x- 1 (a bad choice of method!)
     x = area / pi -1
-    x_sq = x*x
-    x_cubed = x*x*x
-    x_4 = x*x*x*x
-    x_5 = x*x*x*x*x
+    x_2 = x*x
+    x_3 = x_2*x
+    x_4 = x_2*x_2
+    x_5 = x_3*x_2
 
-    return 1 + 1/2*x - 1/8*x_sq + 1/16*x_cubed - 5/128*x_4 + 7/256*x_5
+    return 1 + 1/2*x - 1/8*x_2 + 1/16*x_3 - 5/128*x_4 + 7/256*x_5
 
 #
 #
@@ -16,4 +17,4 @@ def calculate_radius( area):
 
 if __name__ == "__main__":
     my_area = 2
-    print(f'A circle of area {my_area} has a radiys of {calculate_radius(my_area)}')
+    print(f'A circle of area {my_area} has a radius of {calculate_radius(my_area)}')
