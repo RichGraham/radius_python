c2a19ecc0fccbf9a6075b95eaa464198d4a1d658
Richard Graham <Richard.Graham@nottingham.ac.uk>
2022-11-28 09:33:36 +0000

    b1 corrections

--- radius.py
+++ radius.py
@@ -3,12 +3,12 @@ def calculate_radius( area):
 
     # Calculate the square root by a Taylor series in powers of x- 1 (a bad choice of method!)
     x = area / pi -1
-    x_sq = x*x
-    x_cubed = x*x*x
+    x_2 = x*x
+    x_3 = x*x*x
     x_4 = x*x*x*x
     x_5 = x*x*x*x*x
 
-    return 1 + 1/2*x - 1/8*x_sq + 1/16*x_cubed - 5/128*x_4 + 7/256*x_5
+    return 1 + 1/2*x - 1/8*x_2 + 1/16*x_3 - 5/128*x_4 + 7/256*x_5
 
 #
 #
@@ -16,4 +16,4 @@ def calculate_radius( area):
 
 if __name__ == "__main__":
     my_area = 2
-    print(f'A circle of area {my_area} has a radiys of {calculate_radius(my_area)}')
+    print(f'A circle of area {my_area} has a radius of {calculate_radius(my_area)}')
