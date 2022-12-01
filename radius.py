58fea89d298fbda351b09afbe4857d311c752a53
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-12-01 09:55:49 +0000

    Uses built-in sqrt function and other minor fixes

--- radius.py
+++ radius.py
@@ -1,14 +1,8 @@
+import numpy as np
 def calculate_radius( area):
-    pi = 3.14159
+    pi = 3.14159265
 
-    # Calculate the square root by a Taylor series in powers of x- 1 (a bad choice of method!)
-    x = area / pi -1
-    x_sq = x*x
-    x_cubed = x*x*x
-    x_4 = x*x*x*x
-    x_5 = x*x*x*x*x
-
-    return 1 + 1/2*x - 1/8*x_sq + 1/16*x_cubed - 5/128*x_4 + 7/256*x_5
+    return np.sqrt( area/pi)
 
 #
 #
@@ -16,4 +10,4 @@ def calculate_radius( area):
 
 if __name__ == "__main__":
     my_area = 2
-    print(f'A circle of area {my_area} has a radiys of {calculate_radius(my_area)}')
+    print(f'A circle of area {my_area}m^2 has a radiys of {calculate_radius(my_area)}m')
